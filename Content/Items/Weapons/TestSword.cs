8ea4076dc8f8bc58baf23016e272fdaeb87caab6
levi <leviamullins@gmail.com>
2025-02-06 21:05:36 -0500

    Add project files.

---
+++ Content/Items/Weapons/TestSword.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using LevisMod.Content.Projectiles;
+
+namespace LevisMod.Content.Items.Weapons
+{
+    internal class TestSword : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 15;
+            Item.useAnimation = 15;
+            Item.autoReuse = true;
+
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 30;
+            Item.crit = 6;
+
+            Item.value = Item.buyPrice(gold: 10, silver: 50);
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item1;
+
+            Item.shoot = ModContent.ProjectileType<TestSwordProjectile>();
+            Item.shootSpeed = 8;
+        }
+
+        public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.Frostburn2, 360);
+        }
+    }
+}
---
+++ Content/Projectiles/TestSwordProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LevisMod.Content.Projectiles
+{
+    internal class TestSwordProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.aiStyle = 0;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.penetrate = 10;
+            Projectile.timeLeft = 720;
+            Projectile.light = 0.1f;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = true;
+            Projectile.extraUpdates = 0;
+        }
+
+        public override void AI()
+        {
+            Projectile.ai[0] += 1f;
+            if (Projectile.ai[0] % 20 == 0 && Projectile.ai[0] < 200)
+            {
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity, ProjectileID.CursedFlameFriendly, 20, 7);
+            }
+        }
+    }
+}
