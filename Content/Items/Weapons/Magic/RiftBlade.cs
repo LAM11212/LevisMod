fd47ccef0adfce58e13b3fea66ac4d6226a538b1
levi <leviamullins@gmail.com>
2025-02-22 00:47:11 -0500

    added riftblade item

---
+++ Content/Items/Weapons/Magic/RiftBlade.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LevisMod.Content.Projectiles;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LevisMod.Content.Items.Weapons.Magic
+{
+    internal class RiftBlade : ModItem
+    {
+        private bool onCoolDown;
+        private int baseMana = 0;
+        private int manaCap = 150;
+        private int count = 0;
+        private int cooldownStartTime = 0;
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 30;
+            Item.useTime = 20;
+            Item.mana = baseMana;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.noMelee = true;
+            Item.rare = ItemRarityID.Yellow;
+            Item.UseSound = SoundID.Item6;
+            Item.autoReuse = false;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if(onCoolDown)
+            {
+                if(Main.GameUpdateCount - cooldownStartTime < 1200)
+                {
+                    return false;
+                }
+                else
+                {
+                    onCoolDown = false;
+                    count = 0;
+                    Item.mana = baseMana;
+                }
+            }
+            return true;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if(!CanUseItem(player))
+            {
+                return false;
+            }
+
+            Vector2 MousePos = Main.MouseWorld;
+
+            if(!Collision.SolidCollision(MousePos, player.width, player.height))
+            {
+                count++;
+
+                if (count < 10)
+                {
+                    Item.mana = Math.Min(Item.mana * 2, manaCap);
+                } 
+                else
+                {
+                    onCoolDown = true;
+                    cooldownStartTime = (int)Main.GameUpdateCount;
+                }
+                for(int i = 0; i < 70; i++)
+                {
+                    Dust.NewDust(player.position, player.width, player.height, DustID.TeleportationPotion);
+                }
+
+                player.position = MousePos;
+
+                for(int i = 0; i < 70; i++)
+                {
+                    Dust.NewDust(player.position, player.width, player.height, DustID.TeleportationPotion);
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- NPCs/Misato.cs
+++ NPCs/Misato.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,7 @@ namespace LevisMod.NPCs
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCHit1;
             NPC.knockBackResist = 0.5f;
-            Main.npcFrameCount[NPC.type] = 21; // match with aseprite frames
+            Main.npcFrameCount[NPC.type] = 25; // match with aseprite frames
             NPCID.Sets.ExtraFramesCount[NPC.type] = 0;
             NPCID.Sets.AttackFrameCount[NPC.type] = 1;
             NPCID.Sets.DangerDetectRange[NPC.type] = 600;
@@ -36,5 +37,64 @@ namespace LevisMod.NPCs
             NPCID.Sets.HatOffsetY[NPC.type] = 4;
             AnimationType = NPCID.Dryad;
         }
+
+        public override bool CanTownNPCSpawn(int numTownNPCs)
+        {
+            for(var i = 0; 9 < 255; i++)
+            {
+                Player player = Main.player[i];
+                foreach (Item item in player.inventory)
+                {
+                    if (item.type == ModContent.ItemType<ProgressiveKnife>())
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        public override List<String> SetNPCNameList()
+        {
+            return new List<String>()
+            {
+                "Misato",
+                "Misato-San"
+            };
+        }
+
+        public override void SetChatButtons(ref string button, ref string button2)
+        {
+            button = "Shop";
+            button2 = "Talk";
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback)
+        {
+            damage = 30;
+            knockback = 5f;
+        }
+
+        public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
+        {
+            cooldown = 8;
+            randExtraCooldown = 10;
+        }
+
+        public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
+        {
+            projType = ProjectileID.WoodenArrowFriendly;
+            attackDelay = 1;
+        }
+
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+        {
+            multiplier = 7f;
+        }
+
+        public override void OnKill()
+        {
+            Item.NewItem(NPC.GetSource_Death(), NPC.getRect(), ItemID.CloudinaBottle);
+        }
     }
+    //TODO implement rest of NPC
 }
